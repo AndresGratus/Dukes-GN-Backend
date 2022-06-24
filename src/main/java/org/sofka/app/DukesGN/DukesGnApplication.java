0be3e2f9a04c8ceacdc08dbc639b2768c3e0942f
juantoro7295 <juanpatoro2011@gmail.com>
2022-06-24 15:53:36 -0500

    Backend - Proyecto Base
    Se crea el proyecto base con todas las dependencias necesarias

---
+++ src/main/java/org/sofka/app/DukesGN/DukesGnApplication.java
@@ -0,0 +1,13 @@
+package org.sofka.app.DukesGN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukesGnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DukesGnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sofka/app/DukesGN/DukesGnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sofka.app.DukesGN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DukesGnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
