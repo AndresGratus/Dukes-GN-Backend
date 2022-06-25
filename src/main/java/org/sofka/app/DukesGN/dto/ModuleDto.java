cf516ffdd79a2f1dd3b8a87803f263484a0356c5
DMosqueral <dml2410@gmail.com>
2022-06-25 10:51:41 -0500

    [FEAT] Validaciones y Excepciones ModuleDto
    Se implementan las excepciones y validaciones para ModuleDto
    
    HU - Validación de datos

--- src/main/java/org/sofka/app/DukesGN/dto/ModuleDto.java
+++ src/main/java/org/sofka/app/DukesGN/dto/ModuleDto.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.sofka.app.DukesGN.util.exception.ValidateArgument;
+import org.sofka.app.DukesGN.util.message.Messages;
 
 @Getter
 @Setter
-@AllArgsConstructor
+//@AllArgsConstructor
 @NoArgsConstructor
 public class ModuleDto {
     private String id_module;
@@ -15,4 +17,23 @@ public class ModuleDto {
     private String name;
     private Double score;
     private Double percentage;
+
+    public ModuleDto(String id_module, String id_course, String name, Double score, Double percentage) {
+        ValidateArgument.validateStringNull(this.id_module, Messages.ID_MODULO_NULO);
+        this.id_module = id_module;
+
+        ValidateArgument.validateStringNull(this.id_course, Messages.ID_CURSO_NULO);
+        this.id_course = id_course;
+
+        ValidateArgument.validateStringEmpty(this.name, Messages.NOMBRE_MODULO_VACIO);
+        this.name = name;
+
+        ValidateArgument.valiteNumberNegative(this.score, Messages.VALOR_NOTA_MINIMO);
+        ValidateArgument.validateNumberMax(this.score, Messages.VALOR_NOTA_MAXIMO);
+        this.score = score;
+
+        ValidateArgument.valiteNumberNegative(this.percentage, Messages.VALOR_PORCENTAJE_MINIMO);
+        ValidateArgument.validateNumberMax(this.percentage, Messages.VALOR_PORCENTAJE_MAXIMO);
+        this.percentage = percentage;
+    }
 }
