ec191a81d7aa32856634624195f460503e41ec8b
Andres <andres.castaneda280040@gmail.com>
2022-06-25 10:45:30 -0500

    [FEAT] Validacion y Excepciones ProgramDto
    Se implementa excepcionesy validaciones para ProgramDto
    
    HU - Validación de datos

--- src/main/java/org/sofka/app/DukesGN/dto/ProgramDto.java
+++ src/main/java/org/sofka/app/DukesGN/dto/ProgramDto.java
@@ -4,13 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.sofka.app.DukesGN.util.exception.ValidateArgument;
+import org.sofka.app.DukesGN.util.message.Messages;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 @Getter
 @Setter
-@AllArgsConstructor
+//@AllArgsConstructor
 @NoArgsConstructor
 public class ProgramDto {
 
@@ -30,4 +32,23 @@ public class ProgramDto {
 
     private ArrayList<String> apprentice;
 
+    public ProgramDto(String id_program, String name, LocalDateTime start_date, LocalDateTime final_date, String id_coach, String name_coach, ArrayList<String> course, ArrayList<String> apprentice) {
+        ValidateArgument.validateStringNull(this.id_program, Messages.ID_PROGRAMA_NULO);
+        this.id_program = id_program;
+
+        ValidateArgument.validateStringNull(this.name,Messages.NOMBRE_PROGRAMA_VACIO);
+        this.name = name;
+
+        this.start_date = start_date;
+        this.final_date = final_date;
+
+        ValidateArgument.validateStringNull(this.id_coach,Messages.ID_COACH_NULO);
+        this.id_coach = id_coach;
+
+        ValidateArgument.validateStringNull(this.name_coach,Messages.NOMBRE_COACH_VACIO);
+        this.name_coach = name_coach;
+
+        this.course = course;
+        this.apprentice = apprentice;
+    }
 }
