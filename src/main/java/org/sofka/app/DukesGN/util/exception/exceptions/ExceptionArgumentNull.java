1843afe074bc5be16c1bea1dc7d629f201629c66
juantoro7295 <juanpatoro2011@gmail.com>
2022-06-25 10:19:10 -0500

    [FEAT - FIX] Excepciones y Validacion - Correccion Constructor
    Se implementaron las excepciones y validaciones para los dtos y se corrigio el constructor de cursoDto para poder validar los argumentos dentro del constructor
    
    HU - Validacion de datos

---
+++ src/main/java/org/sofka/app/DukesGN/util/exception/exceptions/ExceptionArgumentNull.java
@@ -0,0 +1,6 @@
+package org.sofka.app.DukesGN.util.exception.exceptions;
+
+public class ExceptionArgumentNull extends RuntimeException {
+    public ExceptionArgumentNull(String message) {
+    }
+}
---
+++ src/main/java/org/sofka/app/DukesGN/util/exception/exceptions/ExceptionNumberMax.java
@@ -0,0 +1,6 @@
+package org.sofka.app.DukesGN.util.exception.exceptions;
+
+public class ExceptionNumberMax extends RuntimeException {
+    public ExceptionNumberMax(String message) {
+    }
+}
