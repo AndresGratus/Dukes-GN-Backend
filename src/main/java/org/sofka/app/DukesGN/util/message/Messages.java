4bb51a4dda858fc0efa5d8fa975afccb39c34fa4
juantoro7295 <juanpatoro2011@gmail.com>
2022-06-25 08:54:50 -0500

    [FEAT] Estructura de mensajes
    Se Crea la estructura de mensajes de los Dtos
    
    HU - Validacion de datos

---
+++ src/main/java/org/sofka/app/DukesGN/util/message/Messages.java
@@ -0,0 +1,23 @@
+package org.sofka.app.DukesGN.util.message;
+
+public class Messages {
+
+    /**
+     * Mensajes de curso
+     */
+    public static final String NOMBRE_CURSO_VACIO = "El campo nombre del curso no puede estar vacio";
+
+
+    /**
+     * Mensajes De Programa
+     */
+
+    public  static  final String NOMBRE_PROGRAMA_VACIO = "El campo nombre del programa no puede estar vacio";
+
+    /**
+     * Mensajes de modulos
+     */
+
+    public static final  String NOMBRE_MODULO_VACIO = "EL campo nombre del modulo no puede estar vacio";
+    
+}
