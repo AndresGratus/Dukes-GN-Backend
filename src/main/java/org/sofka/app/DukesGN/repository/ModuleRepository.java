0c697b866cc942c11a910ad9d1e515acecdc799c
juantoro7295 <juanpatoro2011@gmail.com>
2022-06-25 14:29:01 -0500

    [FEAT - FIX] Controlador Heroku Deploy - Solucion Repository
    Se implemento un controlador para evidenciar de forma visual el despliegue de heroku, y  ademas se soluciono un error en el repository de una importacion incorrecta
    
    HU - Controlador Heroku HU - Repositorio Colecciones

--- src/main/java/org/sofka/app/DukesGN/repository/ModuleRepository.java
+++ src/main/java/org/sofka/app/DukesGN/repository/ModuleRepository.java
@@ -1,9 +1,9 @@
 package org.sofka.app.DukesGN.repository;
 
+import org.sofka.app.DukesGN.collection.Module;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ModuleRepository extends ReactiveMongoRepository<Module, String> {
-
 }
