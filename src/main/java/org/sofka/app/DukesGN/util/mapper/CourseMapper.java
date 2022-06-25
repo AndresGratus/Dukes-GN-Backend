d3b51466a23159eb55924d99fbdbd59eb857113e
juantoro7295 <juanpatoro2011@gmail.com>
2022-06-25 06:54:12 -0500

    [FEAT] Creacion Util Mapper Course
    Se crea mapeo de datos para course en sus dos casos
    
    HU - Mapeo manual de datos

---
+++ src/main/java/org/sofka/app/DukesGN/util/mapper/CourseMapper.java
@@ -0,0 +1,41 @@
+package org.sofka.app.DukesGN.util.mapper;
+
+import org.sofka.app.DukesGN.collection.Course;
+import org.sofka.app.DukesGN.dto.CourseDto;
+
+import java.util.function.Function;
+
+public class CourseMapper {
+
+    /**
+     * Metodo que nos permite transformar la colección curso a su equivalente en Dto
+     *
+     * @return CourseDto
+     */
+    public Function<Course, CourseDto> fromCourseToCourseDto() {
+        return course -> {
+            CourseDto courseDto = new CourseDto();
+            courseDto.setId_course(course.getId_course());
+            courseDto.setId_program(course.getId_program());
+            courseDto.setName(course.getName());
+            return courseDto;
+        };
+    }
+
+    /**
+     * Metodo que nos permite transformar el Dto curso a su equivalente en la Colección
+     *
+     * @return Course
+     */
+    public Function<CourseDto, Course> fromCourseDtoToCourse() {
+        return courseDto -> {
+            Course course = new Course();
+            course.setId_course(courseDto.getId_course());
+            course.setId_program(courseDto.getId_program());
+            course.setName(courseDto.getName());
+            return course;
+        };
+    }
+
+
+}
