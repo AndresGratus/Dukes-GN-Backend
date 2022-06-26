6d4a58eff54467d502c2f7ecbf40ca837543ee19
juantoro7295 <juanpatoro2011@gmail.com>
2022-06-26 07:43:09 -0500

    [FEAT] Implementacion Servicios Course
    Se implementan los servicios de course CRD
    
    HU - CRD cursos

---
+++ src/main/java/org/sofka/app/DukesGN/service/implementation/CourseService.java
@@ -0,0 +1,71 @@
+package org.sofka.app.DukesGN.service.implementation;
+
+import org.sofka.app.DukesGN.dto.CourseDto;
+import org.sofka.app.DukesGN.repository.CourseRepository;
+import org.sofka.app.DukesGN.service.ICourseService;
+import org.sofka.app.DukesGN.util.mapper.CourseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class CourseService implements ICourseService {
+    /**
+     * Se inyecta el repositorio de course
+     */
+    @Autowired
+    private CourseRepository courseRepository;
+
+    /**
+     * Se inyecta el mapper de course
+     */
+    private CourseMapper courseMapper;
+
+
+    /**
+     * Servicio para crear un curso
+     *
+     * @param courseDto
+     * @return Mono<CourseDto>
+     */
+    @Override
+    public Mono<CourseDto> createCourse(CourseDto courseDto) {
+        return courseRepository
+                .save(courseMapper
+                        .fromCourseDtoToCourse()
+                        .apply(courseDto))
+                .map(courseMapper
+                        .fromCourseToCourseDto());
+    }
+
+    /**
+     * Servicio para obtener un course
+     *
+     * @param id_course
+     * @return Mono<CourseDto>
+     */
+    @Override
+    public Mono<CourseDto> getCourse(String id_course) {
+        return courseRepository
+                .findById(id_course)
+                .map(courseMapper
+                        .fromCourseToCourseDto());
+    }
+
+    @Override
+    public Flux<CourseDto> listCourses(String id_coach) {
+        return null;
+    }
+
+    /**
+     * Servicio para eliminar un course
+     * @param id_course
+     * @return Mono<Void>
+     */
+    @Override
+    public Mono<Void> deleteCourse(String id_course) {
+        return courseRepository
+                .deleteById(id_course);
+    }
+}
