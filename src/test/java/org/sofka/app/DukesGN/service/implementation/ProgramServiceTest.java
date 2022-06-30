76c186cd7ab72d54d322d4bd31f98489ebd04b58
DMosqueral <dml2410@gmail.com>
2022-06-30 04:47:39 -0500

    [TEST] Tests ProgramService
    Se implementaron los tests para cada uno de los métodos de la clase ProgramService.
    
    HU - Testeo Servicios del Programa

---
+++ src/test/java/org/sofka/app/DukesGN/service/implementation/ProgramServiceTest.java
@@ -0,0 +1,242 @@
+package org.sofka.app.DukesGN.service.implementation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.sofka.app.DukesGN.collection.Program;
+import org.sofka.app.DukesGN.dto.ProgramDto;
+import org.sofka.app.DukesGN.util.mapper.ProgramMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class ProgramServiceTest {
+
+    @Autowired
+    ProgramService programService;
+
+    ProgramMapper programMapper;
+
+    @BeforeEach
+    void setUp() {
+        programService = mock(ProgramService.class);
+    }
+
+    @Test
+    void createProgram() {
+
+        var program = new Program();
+        program.setId_program("P-0001");
+        program.setName("Desarrollo de Software");
+        program.setStart_date(LocalDate.now());
+        program.setFinal_date(LocalDate.of(2022, 07, 15));
+        program.setId_coach("C-0001");
+        program.setName_coach("Julián Lasso");
+        program.setDuration_day(15L);
+        program.setCourse_amount(4D);
+        program.setCourse_percentage(25D);
+
+        //var programDto1 = programMapper.fromProgramtoProgramDto().apply(program);
+
+        var programDto = new ProgramDto();
+        programDto.setId_program(program.getId_program());
+        programDto.setName(program.getName());
+        programDto.setStart_date(program.getStart_date());
+        programDto.setFinal_date(program.getFinal_date());
+        programDto.setId_coach(program.getId_program());
+        programDto.setName_coach(program.getName_coach());
+        programDto.setDuration_day(program.getDuration_day());
+        programDto.setCourse_amount(program.getCourse_amount());
+        programDto.setCourse_percentage(program.getCourse_percentage());
+
+        when(programService.createProgram(programDto))
+                .thenReturn(Mono.just(programDto));
+
+        StepVerifier.create(programService.createProgram(programDto))
+                .expectSubscription()
+                .expectNext(programDto)
+                .verifyComplete();
+
+        verify(programService).createProgram(programDto);
+    }
+
+    @Test
+    void getProgram() {
+
+        var program = new Program();
+        program.setId_program("P-0001");
+        program.setName("Desarrollo de Software");
+        program.setStart_date(LocalDate.now());
+        program.setFinal_date(LocalDate.of(2022, 07, 15));
+        program.setId_coach("C-0001");
+        program.setName_coach("Julián Lasso");
+        program.setDuration_day(15L);
+        program.setCourse_amount(4D);
+        program.setCourse_percentage(25D);
+
+        var programDto = new ProgramDto();
+        programDto.setId_program(program.getId_program());
+        programDto.setName(program.getName());
+        programDto.setStart_date(program.getStart_date());
+        programDto.setFinal_date(program.getFinal_date());
+        programDto.setId_coach(program.getId_program());
+        programDto.setName_coach(program.getName_coach());
+        programDto.setDuration_day(program.getDuration_day());
+        programDto.setCourse_amount(program.getCourse_amount());
+        programDto.setCourse_percentage(program.getCourse_percentage());
+
+        when(programService.getProgram(programDto.getId_program()))
+                .thenReturn(Mono.just(programDto));
+
+        StepVerifier.create(programService.getProgram(programDto.getId_program()))
+                .expectSubscription()
+                .expectNext(programDto)
+                .verifyComplete();
+
+        verify(programService).getProgram(programDto.getId_program());
+    }
+
+    @Test
+    void deleteProgram() {
+
+        var program = new Program();
+        program.setId_program("P-0001");
+        program.setName("Desarrollo de Software");
+        program.setStart_date(LocalDate.now());
+        program.setFinal_date(LocalDate.of(2022, 07, 15));
+        program.setId_coach("C-0001");
+        program.setName_coach("Julián Lasso");
+        program.setDuration_day(15L);
+        program.setCourse_amount(4D);
+        program.setCourse_percentage(25D);
+
+        var programDto = new ProgramDto();
+        programDto.setId_program(program.getId_program());
+        programDto.setName(program.getName());
+        programDto.setStart_date(program.getStart_date());
+        programDto.setFinal_date(program.getFinal_date());
+        programDto.setId_coach(program.getId_program());
+        programDto.setName_coach(program.getName_coach());
+        programDto.setDuration_day(program.getDuration_day());
+        programDto.setCourse_amount(program.getCourse_amount());
+        programDto.setCourse_percentage(program.getCourse_percentage());
+
+        when(programService.deleteProgram(programDto.getId_program()))
+                .thenReturn(Mono.empty());
+
+        StepVerifier.create(programService.deleteProgram(programDto.getId_program()))
+                .expectSubscription()
+                .expectNext()
+                .verifyComplete();
+
+        verify(programService).deleteProgram(programDto.getId_program());
+    }
+
+    @Test
+    void listAllProgram() {
+
+        var program = new Program();
+        program.setId_program("P-0001");
+        program.setName("Desarrollo de Software");
+        program.setStart_date(LocalDate.now());
+        program.setFinal_date(LocalDate.of(2022, 07, 15));
+        program.setId_coach("C-0001");
+        program.setName_coach("Julián Lasso");
+        program.setDuration_day(15L);
+        program.setCourse_amount(4D);
+        program.setCourse_percentage(25D);
+
+        var programDto = new ProgramDto();
+        programDto.setId_program(program.getId_program());
+        programDto.setName(program.getName());
+        programDto.setStart_date(program.getStart_date());
+        programDto.setFinal_date(program.getFinal_date());
+        programDto.setId_coach(program.getId_program());
+        programDto.setName_coach(program.getName_coach());
+        programDto.setDuration_day(program.getDuration_day());
+        programDto.setCourse_amount(program.getCourse_amount());
+        programDto.setCourse_percentage(program.getCourse_percentage());
+
+        when(programService.listAllProgram()).thenReturn(Flux.just());
+
+        StepVerifier.create(programService.listAllProgram())
+                .expectSubscription()
+                .expectNextMatches( p -> {
+                    assert p.getName().equals(program.getName());
+                    return true;
+                })
+                .expectComplete();
+
+        verify(programService).listAllProgram();
+    }
+
+    @Test
+    void saveAllProgram() {
+
+        var program1 = new Program();
+        program1.setId_program("P-0001");
+        program1.setName("Desarrollo de Software");
+        program1.setStart_date(LocalDate.now());
+        program1.setFinal_date(LocalDate.of(2022, 07, 15));
+        program1.setId_coach("C-0001");
+        program1.setName_coach("Julián Lasso");
+        program1.setDuration_day(15L);
+        program1.setCourse_amount(4D);
+        program1.setCourse_percentage(25D);
+
+        var program1Dto = new ProgramDto();
+        program1Dto.setId_program(program1.getId_program());
+        program1Dto.setName(program1.getName());
+        program1Dto.setStart_date(program1.getStart_date());
+        program1Dto.setFinal_date(program1.getFinal_date());
+        program1Dto.setId_coach(program1.getId_program());
+        program1Dto.setName_coach(program1.getName_coach());
+        program1Dto.setDuration_day(program1.getDuration_day());
+        program1Dto.setCourse_amount(program1.getCourse_amount());
+        program1Dto.setCourse_percentage(program1.getCourse_percentage());
+
+        var program2 = new Program();
+        program2.setId_program("P-0001");
+        program2.setName("Desarrollo de Software");
+        program2.setStart_date(LocalDate.now());
+        program2.setFinal_date(LocalDate.of(2022, 07, 15));
+        program2.setId_coach("C-0001");
+        program2.setName_coach("Julián Lasso");
+        program2.setDuration_day(15L);
+        program2.setCourse_amount(4D);
+        program2.setCourse_percentage(25D);
+
+        var program2Dto = new ProgramDto();
+        program2Dto.setId_program(program2.getId_program());
+        program2Dto.setName(program2.getName());
+        program2Dto.setStart_date(program2.getStart_date());
+        program2Dto.setFinal_date(program2.getFinal_date());
+        program2Dto.setId_coach(program2.getId_program());
+        program2Dto.setName_coach(program2.getName_coach());
+        program2Dto.setDuration_day(program2.getDuration_day());
+        program2Dto.setCourse_amount(program2.getCourse_amount());
+        program2Dto.setCourse_percentage(program2.getCourse_percentage());
+
+        var listProgramsDto = List.of(program1Dto, program2Dto);
+
+        when(programService.saveAllProgram(listProgramsDto))
+                .thenReturn(Flux.just(listProgramsDto));
+
+        StepVerifier.create(programService.saveAllProgram(listProgramsDto))
+                .expectSubscription()
+                .expectNext(listProgramsDto)
+                .verifyComplete();
+
+        verify(programService).saveAllProgram(listProgramsDto);
+    }
+}
